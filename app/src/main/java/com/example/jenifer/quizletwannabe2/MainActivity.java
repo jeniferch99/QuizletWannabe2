f74a6befbc2a99bf75d827b5f04ba6051f8a0cfb
Jenifer <jeniferch99@gmail.com>
2018-10-13 16:18:03 -0700

    Initial commit

---
+++ app/src/main/java/com/example/jenifer/quizletwannabe2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.jenifer.quizletwannabe2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setVisibility(View.VISIBLE);
+            }
+        });
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                v.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
